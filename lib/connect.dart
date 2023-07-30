fe644a70881911e6c6889a0cbea6f47ebc5f6fa9
sounakbanerjee <sounakbanerjee1993@gmail.com>
2023-07-31 02:08:02 +0530

    First BLE Project (AirSense)

--- lib/connect.dart
+++ lib/connect.dart
@@ -5,6 +5,7 @@ import 'dart:convert' show utf8;
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
 
 class ConnectPage extends StatefulWidget {
   final DiscoveredDevice device;
@@ -17,64 +18,141 @@ class ConnectPage extends StatefulWidget {
 
 class _ConnectPageState extends State<ConnectPage> {
   bool isDeviceConnected = false;
+  bool isRecordStarted = false;
+  bool stopTimer = false;
   final _ble = FlutterReactiveBle();
   bool isHeadingAdded = false; // Flag to track if headings are added
+  String selectedEnvironment = 'None';
+  String selectedActivity = 'None';
 
-  Future<void> connectToDevice(DiscoveredDevice device, int fileNumber) async {
+  Future<void> connectToDevice(DiscoveredDevice device) async {
     try {
       final deviceConnection = _ble.connectToDevice(
         id: device.id,
-        connectionTimeout: Duration(seconds: 10),
+        connectionTimeout: const Duration(seconds: 10),
       );
       await deviceConnection;
       print('Connected to ${device.name}');
 
       // Set the isDeviceConnected state to true after successful connection
-      setState(() {
-        isDeviceConnected = true;
+      Timer(Duration(seconds: 1), () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Connected Successfully'),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isDeviceConnected = true;
+                    });
+                    Navigator.pop(context); // Close the dialog
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
       });
-
-      final services = await _ble.discoverServices(device.id);
-      for (var service in services) {
-        if (service.serviceId == Uuid.parse("0000181a-0000-1000-8000-00805f9b34fb")) {
-          final characteristics = service.characteristics;
-
-          for (var characteristic in characteristics) {
-            //print('Characs are : $characteristic');
-
-            if (characteristic.characteristicId == Uuid.parse("00002a6e-0000-1000-8000-00805f9b34fb")) {
-              final qualifiedCharacteristic = QualifiedCharacteristic(
-                deviceId: device.id,
-                serviceId: service.serviceId,
-                characteristicId: characteristic.characteristicId,
-              );
-              // Read from the characteristic
-              for (var i = 0; i<3; i++) {
-
-                final response = await _ble.readCharacteristic(
-                    qualifiedCharacteristic);
-                if (response.isNotEmpty) {
-                  final value = response[0];
-                  print('Characteristic Value: ${value}');
-                  // Write value to CSV file
-                  await writeToCSV(value,fileNumber);
-                  sleep(const Duration(seconds: 4)); // Notify
-                  //await readCSVData();
-                } else {
-                  print('Error reading characteristic value');
-                }
-              }
-              await readCSVData(fileNumber);
-            } //
-          }
-        }
-      }
     }catch (error) {
       print('Error connecting or reading characteristic: $error');
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Connection Failed'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Close the dialog
+                  setState(() {
+                    isDeviceConnected = false;
+                  });
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
-  Future<void> writeToCSV(int value, int fileNumber) async {
+  void checkRecording() {
+    if (selectedEnvironment == 'None' || selectedActivity == 'None') {
+      // Show an alert dialog indicating invalid entry
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Invalid Entry'),
+            content: Text('Please select valid attributes from the dropdown lists.'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Close the dialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      setState(() {
+        isRecordStarted = true;
+      });
+      // Create the File Name with current date and time, selectedEnvironment, and selectedActivity
+      String fileName = '${DateFormat('yyyy-MM-dd_HH:mm:ss').format(DateTime.now())}_$selectedEnvironment\_$selectedActivity';
+      print('CSV FILE NAME: $fileName');
+      startRecording(widget.device, fileName);
+    }
+  }
+
+  Future<void> startRecording(DiscoveredDevice device, String fileName) async {
+
+    final services = await _ble.discoverServices(device.id);
+
+    for (var service in services) {
+      if (service.serviceId == Uuid.parse("0000181a-0000-1000-8000-00805f9b34fb")) {
+        final characteristics = service.characteristics;
+
+        for (var characteristic in characteristics) {
+        //print('Characs are : $characteristic');
+
+          if (characteristic.characteristicId == Uuid.parse("00002a6e-0000-1000-8000-00805f9b34fb")) {
+            final qualifiedCharacteristic = QualifiedCharacteristic(
+              deviceId: device.id,
+              serviceId: service.serviceId,
+              characteristicId: characteristic.characteristicId,
+            );
+            // Read from the characteristic
+            while (!stopTimer) {
+              final response = await _ble.readCharacteristic(
+                  qualifiedCharacteristic);
+              if (response.isNotEmpty) {
+                final value = response[0];
+                print('Characteristic Value: ${value}');
+                // Write value to CSV file
+                await writeToCSV(value,fileName);
+                sleep(const Duration(seconds: 4)); // Notify
+                //await readCSVData();
+              } else {
+                print('Error reading characteristic value');
+              }
+            }
+            await readCSVData(fileName);
+          } //
+        }
+      }
+    }
+  }
+
+
+  Future<void> writeToCSV(int value, String fileName) async {
+
     final List<List<dynamic>> rows = [];
 
     // Check if headings are already added
@@ -89,7 +167,7 @@ class _ConnectPageState extends State<ConnectPage> {
     String csv = ListToCsvConverter().convert(rows);
 
     final String dir = (await getExternalStorageDirectory())!.path;
-    final String path = '$dir/$fileNumber.csv';
+    final String path = '$dir/$fileName.csv';
 
     final File file = File(path);
 
@@ -111,10 +189,10 @@ class _ConnectPageState extends State<ConnectPage> {
     }
   }
 
-  Future<void> readCSVData(int fileNumber) async {
+  Future<void> readCSVData(String fileName) async {
     try {
       final String dir = (await getExternalStorageDirectory())!.path;
-      final String path = "$dir/$fileNumber.csv";
+      final String path = "$dir/$fileName.csv";
       final File file = File(path);
 
       if (await file.exists()) {
@@ -137,23 +215,11 @@ class _ConnectPageState extends State<ConnectPage> {
     }
   }
 
-  int generateUniqueNumber() {
-    // Get the current date and time
-    DateTime now = DateTime.now();
-    // Convert the current date and time to seconds since the epoch
-    int secondsSinceEpoch = now.second;
-    //Use the secondsSinceEpoch as a unique number
-    return secondsSinceEpoch;
-  }
-
   @override
   Widget build(BuildContext context) {
-    int uniqueFileNumber = generateUniqueNumber();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
-          title: Text('Second Page'),
+          title: Text('Data Collection'),
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
             onPressed: () {
@@ -163,26 +229,97 @@ class _ConnectPageState extends State<ConnectPage> {
         ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              ElevatedButton(
-                onPressed: isDeviceConnected
-                    ? null
-                    : () {
-                  connectToDevice(widget.device, uniqueFileNumber);
-                },
-                child: Text('Connect to BLE Device'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: isDeviceConnected
+                            ? null
+                            : () {
+                          connectToDevice(widget.device);
+                        },
+                        child: Text('Connect to BLE Device'),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: stopTimer ?
+                          () {
+                        print('Reset Everything');
+                        setState(() {
+                          isRecordStarted = false;
+                          stopTimer = false;
+                        });
+                        } : null,
+                      icon: Icon(Icons.refresh),
+                    ),
+                  ],
+                ),
               ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DropdownButton<String>(
+                    value: selectedEnvironment,
+                    onChanged: isDeviceConnected && !isRecordStarted
+                        ? (String? newValue) {
+                      setState(() {
+                        selectedEnvironment = newValue ?? 'None';
+                      });
+                    } : null,
+                    items: <String>['None', 'Indoor', 'Outdoor']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(width: 20),
+                  DropdownButton<String>(
+                    value: selectedActivity,
+                    onChanged: isDeviceConnected && !isRecordStarted
+                        ? (String? newValue) {
+                      setState(() {
+                        selectedActivity = newValue ?? 'None';
+                      });
+                    } : null,
+                    items: <String>['None', 'Running', 'Walking', 'Sitting']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
               ElevatedButton(
-                onPressed: (){
-                  print('Start recording');
-                },
-                child: Text('Start recording'),
+                onPressed: isDeviceConnected && !isRecordStarted
+                    ? () {
+                  checkRecording();
+                } : null,
+                child: Text('Start Recording'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: isRecordStarted && !stopTimer
+                    ? () {
+                  print('Stop Recording function');
+                  setState(() {
+                    stopTimer = true;
+                  });
+                } : null,
+                child: Text('Stop Recording'),
               ),
             ],
           ),
         ),
-      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:bluetooth_enable_fork/bluetooth_enable_fork.dart';
 import 'connect.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,9 +17,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color(0xFFEEE2D4),
+        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Flutter React Bluetooth'),
     );
@@ -39,28 +42,49 @@ class _MyHomePageState extends State<MyHomePage> {
   StreamSubscription? _scanSubscription;
 
   @override
+  // void initState() {
+  //   super.initState();
+  //   scanDevices();
+  // }
   void dispose() {
     _scanSubscription?.cancel();
     super.dispose();
   }
 
   Future<void> scanDevices() async{
-    // Request to turn on Bluetooth within an app
-    BluetoothEnable.enableBluetooth.then((result) {
-      if (result == "true") {
-        // Bluetooth has been enabled
-      }
-      else if (result == "false") {
-        // Bluetooth has not been enabled
-      }
-    });
-    // Request Bluetooth permissions
+    Location location = Location();
+    // Request permissions
     var bluetoothScanPermission = await Permission.bluetoothScan.request();
     var locationPermission = await Permission.location.request();
     var bluetoothConnectPermission = await Permission.bluetoothConnect.request();
 
     if (bluetoothScanPermission.isGranted && locationPermission.isGranted && bluetoothConnectPermission.isGranted) {
       // Permission granted, start scanning for devices
+
+      // Request to turn on Bluetooth within an app
+      String bluetoothResult = await BluetoothEnable.enableBluetooth;
+      if (bluetoothResult == "true") {
+        // Bluetooth has been enabled
+        print('Bluetooth has been enabled.');
+      } else if (bluetoothResult == "false") {
+        // Bluetooth has not been enabled
+        print('Bluetooth has not been enabled.');
+      }
+
+      // Request to turn on Location within an app
+      bool serviceEnabled = await location.serviceEnabled();
+      if (!serviceEnabled) {
+        // Request to enable location services
+        serviceEnabled = await location.requestService();
+        if (!serviceEnabled) {
+          // Location services are still not enabled
+          print('Location services are still not enabled.');
+        }
+      } else {
+        // Location services are already enabled
+        print('Location services are already enabled.');
+      }
+
       _scanSubscription?.cancel();
       _scanSubscription = _ble.scanForDevices(
         withServices: [],
@@ -83,44 +107,68 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('BLE Scanner'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: scanDevices,
+                  style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFEACAA3), foregroundColor: Colors.black),
+                  child: Text('Scan Devices'),
+                ),
+              ),
+            ),
+          ],
         ),
         body: Column(
           children: <Widget>[
-            ElevatedButton(
-              onPressed: scanDevices,
-              child: Text('Scan for Devices'),
-            ),
             Expanded(
               child: ListView.builder(
                 itemCount: scanResults.length,
                 itemBuilder: (context, index) {
                   DiscoveredDevice device = scanResults[index];
-                  return ListTile(
-                    title: Text(device.name ?? 'Unknown'),
-                    subtitle: Text(device.id.toString()),
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ConnectPage(device: device), // Pass the device as an argument
+                  //String deviceName = device.name ?? 'Unknown Device';
+                  return Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFEACAA3),
+                        borderRadius: BorderRadius.circular(10.0), 
+                      ),
+                      child: ListTile(
+                        title: Text(device.name ?? 'Unknown Device'),
+                        subtitle: Text(device.id.toString()),
+                        trailing: ElevatedButton(
+                          onPressed: (){
+                            print('Dummy Connect');
+                          },
+                          style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFB47A36)),
+                          child: Text('Connect'),
                         ),
-                      ); // Navigator
-                      // print('Number : $uniqueFileNumber');
-                      // connectToDevice(device,uniqueFileNumber);
-                    },
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ConnectPage(device: device), // Pass the device as an argument
+                            ),
+                          ); // Navigator
+                          // print('Number : $uniqueFileNumber');
+                          // connectToDevice(device,uniqueFileNumber);
+                        },
+                      ),
+                    ),
                   );
                 },
               ),
             ),
           ],
         ),
-      ),
     );
   }
 
